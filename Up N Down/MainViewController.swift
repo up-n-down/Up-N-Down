50362726300ef5dae48d281e8424fa9494ee6dd6
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-10-16 21:05:26 +0200

    Add simple container app with setup instructions

---
+++ Up N Down/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Up N Down
+//
+//  Created by Thomas Paul Mann on 16/10/2016.
+//  Copyright © 2016 Thomas Paul Mann. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidAppear() {
+        super.viewDidAppear()
+
+          }
+
+    // MARK: Actions
+
+    @IBAction func openPreferencesDidPress(_ sender: NSButton) {
+        let extensionsPreferences = URL(fileURLWithPath: "/System/Library/PreferencePanes/Extensions.prefPane")
+        NSWorkspace.shared().open(extensionsPreferences)
+    }
+
+}
+
