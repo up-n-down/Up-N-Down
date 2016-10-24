9ed0ae1d404c6d3289085fecd6dd3ac670f3225d
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-10-24 21:21:48 +0200

    Add on boarding scene

---
+++ App/OnBoardingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OnBoardingViewController.swift
+//  Up N Down
+//
+//  Created by Thomas Paul Mann on 24/10/2016.
+//  Copyright © 2016 Thomas Paul Mann. All rights reserved.
+//
+
+import Cocoa
+
+class OnBoardingViewController: NSViewController {
+    
+}
